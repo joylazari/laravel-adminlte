1d9285a2d85b6f26a72e174e3b0181af0ba2f953
archytas <r.emmerink@archytas.nl>
2015-05-21 02:06:44 +0200

    removed duplicate assets, added dropdown toggle

--- src/views/_layout/js.blade.php
+++ src/views/_layout/js.blade.php
@@ -1,12 +1,9 @@
-<script src="{{ URL::asset('packages/almasaeed2010/adminlte/plugins/jQuery/jQuery-2.1.3.min.js') }}"></script>
 <script src="http://code.jquery.com/ui/1.11.2/jquery-ui.min.js" type="text/javascript"></script>
 <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
 <script>
     $.widget.bridge('uibutton', $.ui.button);
 </script>
-<script src="{{ URL::asset('packages/almasaeed2010/adminlte/bootstrap/js/bootstrap.min.js') }}" type="text/javascript"></script>
 <script src="http://cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
-<script src="{{ URL::asset('packages/almasaeed2010/adminlte/plugins/morris/morris.min.js') }}" type="text/javascript"></script>
 <script src="{{ URL::asset('packages/almasaeed2010/adminlte/plugins/sparkline/jquery.sparkline.min.js') }}" type="text/javascript"></script>
 <script src="{{ URL::asset('packages/almasaeed2010/adminlte/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js') }}" type="text/javascript"></script>
 <script src="{{ URL::asset('packages/almasaeed2010/adminlte/plugins/jvectormap/jquery-jvectormap-world-mill-en.js') }}" type="text/javascript"></script>
@@ -16,5 +13,8 @@
 <script src="{{ URL::asset('packages/almasaeed2010/adminlte/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js') }}" type="text/javascript"></script>
 <script src="{{ URL::asset('packages/almasaeed2010/adminlte/plugins/iCheck/icheck.min.js') }}" type="text/javascript"></script>
 <script src="{{ URL::asset('packages/almasaeed2010/adminlte/plugins/slimScroll/jquery.slimscroll.min.js') }}" type="text/javascript"></script>
-<script src='packages/almasaeed2010/adminlte/plugins/fastclick/fastclick.min.js'></script>
+<script src="{{ URL::asset('packages/almasaeed2010/adminlte/plugins/fastclick/fastclick.min.js') }}"></script>
 <script src="{{ URL::asset('packages/almasaeed2010/adminlte/dist/js/app.min.js') }}" type="text/javascript"></script>
+<script>
+    $('.dropdown-toggle').dropdown();
+</script>
