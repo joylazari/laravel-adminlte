176f541b2164eb5362daf406e36f46657b8e0461
archytas <r.emmerink@archytas.nl>
2015-05-21 04:17:43 +0200

    cdns over https

--- src/views/_layout/js.blade.php
+++ src/views/_layout/js.blade.php
@@ -3,7 +3,7 @@
 <script>
     $.widget.bridge('uibutton', $.ui.button);
 </script>
-<script src="http://cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
 <script src="{{ URL::asset('packages/almasaeed2010/adminlte/plugins/sparkline/jquery.sparkline.min.js') }}" type="text/javascript"></script>
 <script src="{{ URL::asset('packages/almasaeed2010/adminlte/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js') }}" type="text/javascript"></script>
 <script src="{{ URL::asset('packages/almasaeed2010/adminlte/plugins/jvectormap/jquery-jvectormap-world-mill-en.js') }}" type="text/javascript"></script>
